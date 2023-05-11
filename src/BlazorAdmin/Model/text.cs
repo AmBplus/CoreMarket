c030794f441f5b1526c941376dfea1d5cc73b03b
Mohammad Sadegh Heidari <MohammadHeidari119@gmail.com>
2023-05-11 16:29:47 +0430

    add Search and Add and DataTable Pane componnets for BlazorAdmin

---
+++ src/BlazorAdmin/Model/text.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAdmin.Model
+{
+    public class text
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ImageSrc { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
--- src/BlazorAdmin/Program.cs
+++ src/BlazorAdmin/Program.cs
@@ -4,9 +4,10 @@ using BlazorAdmin;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#admin");
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Logging.ClearProviders();
+builder.Services.AddLogging();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 await builder.Build().RunAsync();
